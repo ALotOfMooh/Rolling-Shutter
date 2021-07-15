c04cbc2ebd2a49e06b278559a40058cabeb7af77
SunSharks <lysanne.rogers@gmail.com>
2021-07-15 14:35:44 +0200

    add directions

--- rs.py
+++ rs.py
@@ -1,14 +1,46 @@
+"""Implementation of Rolling Shutter Effect. For now it works on a desktop PC with attatched camera.
+    """
 import time
 import cv2
 import numpy as np
 
-num_sections = 60
+MODE = "desktop"
+direction = "left_to_right"
+num_sections = 80
 history = []
 
 def test():
-    z = np.ones((1, 100))
+    rgb = np.random.randint(255, size=(1000,700,3),dtype=np.uint8)
+    green = np.zeros((50, 700, 3))
+    green[:,:,1] = 1
+    green *= 255
+    rgb[:green.shape[0], :, :] = green
+
+    cv2.imshow('RGB',rgb)
+    cv2.imshow('green',green)
+
 def replace_sections_iterative():
     global history
+    image2=history[0][:, 0:sec_height]
+    s = time.time()
+    if direction == "top_to_bottom":
+        for i in range(1,num_sections):
+            image2=cv2.vconcat([image2,history[i][sec_height*i:sec_height*(i+1)]])
+    elif direction == "bottom_to_top":
+        for i in range(1,num_sections):
+            image2=cv2.vconcat([image2,history[-i][sec_height*i:sec_height*(i+1)]])
+    elif direction == "left_to_right":
+        for i in range(1,num_sections):
+            image2=cv2.hconcat([image2,history[i][:, sec_height*i:sec_height*(i+1)]])
+    elif direction == "right_to_left":
+        for i in range(1,num_sections):
+            image2=cv2.hconcat([image2,history[-i][:, sec_height*i:sec_height*(i+1)]])
+    t = time.time() - s
+    cv2.imshow("preview", image2)
+
+def replace_sections_by_slicing():
+    global history
+    img = np.zeros(history[0].shape)
 
 
 cv2.namedWindow("preview", cv2.WINDOW_FREERATIO)
@@ -23,22 +55,14 @@ else:
 
 
 sec_height = frame.shape[1] // num_sections
-while rval:
-    s = time.time()
 
+while rval:
     rval, frame = vc.read()
     history.append(frame)
 
     if len(history) > num_sections:
         history.pop(0)
-        image2=history[0][0:sec_height]
-        for i in range(1,num_sections):
-            image2=cv2.vconcat([image2,history[i][sec_height*i:sec_height*(i+1)]])
-        cv2.imshow("preview", image2)
-
-    e = time.time()
-    print(s-e)
-
+        replace_sections_iterative()
 
 
     key = cv2.waitKey(20)
